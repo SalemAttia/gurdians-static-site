52845302a20b31f22f2d17fb7dbe0cb2f7bcb6f7
ahmedsalemA <salem.attia.dk@gmail.com>
2019-06-26 21:30:02 +0200

    update layout and add required pages

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,22 @@
+import React from 'react';
+const Banner = () => {
+    return (
+        <section className="wellcome_area clearfix" id="home">
+              <div id='stars'></div>
+              <div id='stars2'></div>
+              <div id='stars3'></div>
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-12 m-aut text-center">
+                    <img id="logo_transparent" src="/assets/img/bg-img/logo_transparent.png" alt=""/>
+                  </div>
+                </div>
+              </div>
+
+
+
+        </section>
+    );
+}
+
+export default Banner;
--- src/components/layouts/layout.js
+++ src/components/layouts/layout.js
@@ -10,6 +10,8 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -26,19 +28,19 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+      <div>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <div className="copy">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 text-center">
+                            <p>© {new Date().getFullYear()}, Guardians-labs</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
       </div>
     </>
