6920b6098e6104ce0cb3420cb12b8cfaab8679a2
ahmedsalemA <salem.attia.dk@gmail.com>
2019-06-28 21:48:38 +0200

    fixing component errors and add contact component to contact us

--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -1,15 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layouts/layout"
+import Contact from '../components/Contact/Contact'
 import SEO from "../components/seo"
 
 const Contactus = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <SEO title="contact us" />
+    <Contact></Contact>
   </Layout>
 )
 
