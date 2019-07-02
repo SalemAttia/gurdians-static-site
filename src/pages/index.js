f6459ee4673c620148403e92a92ab3819a03cc39
Ehab97 <ehabreda04@gmail.com>
2019-07-02 19:51:54 +0200

    mind set Section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react"
 import Layout from "../components/layouts/layout"
 import Banner from "../components/Banner/Banner"
 import TechStack from "../components/TechStack/TechStack"
+import MindSet from '../components/MindSetComponent/MindSetComponent.js'
 import Blog from "../components/Blog/Blog"
 import SEO from "../components/seo"
 
@@ -13,6 +14,7 @@ const IndexPage = () => (
     <Banner></Banner>
     <TechStack></TechStack>
     <Blog title="recent blog"></Blog>
+    <MindSet title="Mind Set"></MindSet>
   </Layout>
 )
 
